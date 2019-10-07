9bfcedef650b8453b2beb5054de1dd7c5ce3354e
Elly Kitoto <junkmailstoelly@gmail.com>
2019-10-07 09:49:05 +0300

    test create checkbox

--- neatformcore/src/test/java/com/nerdstone/neatformcore/robolectric/utils/TestConstants.kt
+++ neatformcore/src/test/java/com/nerdstone/neatformcore/robolectric/utils/TestConstants.kt
@@ -3,22 +3,147 @@ package com.nerdstone.neatformcore.robolectric.utils
 object TestConstants {
     private const val SAMPLE_DIR = "sample/"
     const val SAMPLE_ONE_FORM_FILE = SAMPLE_DIR + "sample_one_form.json"
-    const val SAMPLE_JSON =
-        "{\n  \"form\": \"Profile\",\n  \"is_multi_step\": true,\n  \"rules_file\": \"rules/yml/sample_one_form_rules.yml\",\n " +
-                " \"count\": 1,\n  \"steps\": [\n    {\n      \"title\": \"Test and counselling\",\n      \"step_number\": 1,\n   " +
-                "   \"fields\": [\n        {\n          \"name\": \"adult\",\n          \"type\": \"edit_text\",\n     " +
-                "     \"properties\": {\n            \"hint\": \"This is an adult\",\n            \"type\": \"name\",\n     " +
-                "       \"text_size\": \"20\"\n          },\n          \"meta_data\": {\n            \"openmrs_entity\": \"\",\n        " +
-                "    \"openmrs_entity_id\": \"\",\n            \"openmrs_entity_parent\": \"\"\n          },\n        " +
-                "  \"validation\": \"length() < 8; !contains(@)\",\n          \"subjects\": \"age:number, child:text\",\n       " +
-                "   \"required_status\": \"True:please add username\"\n        },\n        {\n          \"name\": \"age\",\n    " +
-                "      \"type\": \"edit_text\",\n          \"properties\": {\n            \"hint\": \"Enter your age (will be multiplied by 10)\",\n    " +
-                "        \"type\": \"name\",\n            \"text\": \"0\",\n            \"text_size\": \"18.6\",\n         " +
-                "   \"padding\": \"20\"\n          },\n          \"meta_data\": {\n            \"openmrs_entity\": \"\",\n          " +
-                "  \"openmrs_entity_id\": \"\",\n            \"openmrs_entity_parent\": \"\"\n          },\n      " +
-                "    \"required_status\": \"yes:Please add age\"\n        },\n        {\n          \"name\": \"child\",\n  " +
-                "        \"type\": \"edit_text\",\n          \"properties\": {\n            \"hint\": \"I am a child\",\n       " +
-                "     \"type\": \"name\",\n            \"text_size\": \"20\"\n          },\n          \"meta_data\": {\n   " +
-                "         \"openmrs_entity\": \"\",\n            \"openmrs_entity_id\": \"\",\n            \"openmrs_entity_parent\": \"\"\n    " +
-                "      },\n          \"required_status\": \"yes:Please add age\",\n          \"subjects\": \"age\"\n        }\n      ]\n    }\n  ]\n}"
+    const val SAMPLE_JSON ="{\n" +
+            "  \"form\": \"Profile\",\n" +
+            "  \"is_multi_step\": true,\n" +
+            "  \"rules_file\": \"rules/yml/sample_one_form_rules.yml\",\n" +
+            "  \"count\": 1,\n" +
+            "  \"steps\": [\n" +
+            "    {\n" +
+            "      \"title\": \"Test and counselling\",\n" +
+            "      \"step_number\": 1,\n" +
+            "      \"fields\": [\n" +
+            "        {\n" +
+            "          \"name\": \"adult\",\n" +
+            "          \"type\": \"edit_text\",\n" +
+            "          \"properties\": {\n" +
+            "            \"hint\": \"This is an adult\",\n" +
+            "            \"type\": \"name\",\n" +
+            "            \"text\": \"Catch me if you can\"\n" +
+            "          },\n" +
+            "          \"meta_data\": {\n" +
+            "            \"openmrs_entity\": \"\",\n" +
+            "            \"openmrs_entity_id\": \"\",\n" +
+            "            \"openmrs_entity_parent\": \"\"\n" +
+            "          },\n" +
+            "          \"validation\": \"length() < 8; !contains(@)\",\n" +
+            "          \"subjects\": \"age:number, child:text\",\n" +
+            "          \"required_status\": \"True:please add username\"\n" +
+            "        },\n" +
+            "        {\n" +
+            "          \"name\": \"age\",\n" +
+            "          \"type\": \"edit_text\",\n" +
+            "          \"properties\": {\n" +
+            "            \"hint\": \"Enter your age (will be multiplied by 10)\",\n" +
+            "            \"type\": \"name\",\n" +
+            "            \"padding\": \"8\"\n" +
+            "          },\n" +
+            "          \"meta_data\": {\n" +
+            "            \"openmrs_entity\": \"\",\n" +
+            "            \"openmrs_entity_id\": \"\",\n" +
+            "            \"openmrs_entity_parent\": \"\"\n" +
+            "          },\n" +
+            "          \"required_status\": \"yes:Please add age\"\n" +
+            "        },\n" +
+            "        {\n" +
+            "          \"name\": \"child\",\n" +
+            "          \"type\": \"edit_text\",\n" +
+            "          \"properties\": {\n" +
+            "            \"hint\": \"I am a child\",\n" +
+            "            \"type\": \"name\"\n" +
+            "          },\n" +
+            "          \"meta_data\": {\n" +
+            "            \"openmrs_entity\": \"\",\n" +
+            "            \"openmrs_entity_id\": \"\",\n" +
+            "            \"openmrs_entity_parent\": \"\"\n" +
+            "          },\n" +
+            "          \"required_status\": \"yes:Please add age\",\n" +
+            "          \"subjects\": \"age:number, adult:text, email_subscription:map\"\n" +
+            "        },\n" +
+            "        {\n" +
+            "          \"name\": \"email_subscription\",\n" +
+            "          \"type\": \"checkbox\",\n" +
+            "          \"properties\": {\n" +
+            "            \"text\": \"Subscribe to email notifications\"\n" +
+            "          },\n" +
+            "          \"required_status\": \"yes:Please specify if you want subscription\",\n" +
+            "          \"subjects\": \"age:number\"\n" +
+            "        },\n" +
+            "        {\n" +
+            "          \"name\": \"choose_language\",\n" +
+            "          \"type\": \"multi_choice_checkbox\",\n" +
+            "          \"properties\": {\n" +
+            "            \"text\": \"Pick the languages you are proficient in.\"\n" +
+            "          },\n" +
+            "          \"options\": [\n" +
+            "            {\n" +
+            "              \"name\": \"eng\",\n" +
+            "              \"text\": \"English\",\n" +
+            "              \"meta_data\": {\n" +
+            "                \"openmrs_entity\": \"\",\n" +
+            "                \"openmrs_entity_id\": \"A123123123123\",\n" +
+            "                \"openmrs_entity_parent\": \"\"\n" +
+            "              }\n" +
+            "            },\n" +
+            "            {\n" +
+            "              \"name\": \"french\",\n" +
+            "              \"text\": \"French\",\n" +
+            "              \"meta_data\": {\n" +
+            "                \"openmrs_entity\": \"\",\n" +
+            "                \"openmrs_entity_id\": \"A123123123123\",\n" +
+            "                \"openmrs_entity_parent\": \"\"\n" +
+            "              }\n" +
+            "            },\n" +
+            "            {\n" +
+            "              \"name\": \"kisw\",\n" +
+            "              \"text\": \"Kiswahili\",\n" +
+            "              \"meta_data\": {\n" +
+            "                \"openmrs_entity\": \"\",\n" +
+            "                \"openmrs_entity_id\": \"A123123123123\",\n" +
+            "                \"openmrs_entity_parent\": \"\"\n" +
+            "              }\n" +
+            "            },\n" +
+            "            {\n" +
+            "              \"name\": \"need_help\",\n" +
+            "              \"text\": \"Help me choose\",\n" +
+            "              \"is_exclusive\": true,\n" +
+            "              \"meta_data\": {\n" +
+            "                \"openmrs_entity\": \"\",\n" +
+            "                \"openmrs_entity_id\": \"A918928912\",\n" +
+            "                \"openmrs_entity_parent\": \"\"\n" +
+            "              }\n" +
+            "            },\n" +
+            "            {\n" +
+            "              \"name\": \"none\",\n" +
+            "              \"text\": \"None of the above\",\n" +
+            "              \"is_exclusive\": true,\n" +
+            "              \"meta_data\": {\n" +
+            "                \"openmrs_entity\": \"\",\n" +
+            "                \"openmrs_entity_id\": \"A123123123123\",\n" +
+            "                \"openmrs_entity_parent\": \"\"\n" +
+            "              }\n" +
+            "            }\n" +
+            "          ],\n" +
+            "          \"required_status\": \"yes:Please specify your languages\",\n" +
+            "          \"subjects\": \"email_subscription:map\"\n" +
+            "        },\n" +
+            "        {\n" +
+            "          \"name\": \"language_none\",\n" +
+            "          \"type\": \"edit_text\",\n" +
+            "          \"properties\": {\n" +
+            "            \"hint\": \"Specify your language\",\n" +
+            "            \"type\": \"name\"\n" +
+            "          },\n" +
+            "          \"meta_data\": {\n" +
+            "            \"openmrs_entity\": \"\",\n" +
+            "            \"openmrs_entity_id\": \"\",\n" +
+            "            \"openmrs_entity_parent\": \"\"\n" +
+            "          },\n" +
+            "          \"required_status\": \"true:Please specify language\",\n" +
+            "          \"subjects\": \"choose_language:map\"\n" +
+            "        }\n" +
+            "      ]\n" +
+            "    }\n" +
+            "  ]\n" +
+            "}"
 }
