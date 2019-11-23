a7e5168c21fabc8ce65146246874fc5fe5531e4a
cozej4 <ilakozejumanne@gmail.com>
2019-11-23 19:24:11 +0300

    Updated dependencies

--- sample/src/main/java/com/nerdstone/neatform/form/FormActivity.kt
+++ sample/src/main/java/com/nerdstone/neatform/form/FormActivity.kt
@@ -8,12 +8,15 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.nerdstone.neatandroidstepper.core.domain.StepperActions
+import com.nerdstone.neatandroidstepper.core.stepper.Step
+import com.nerdstone.neatandroidstepper.core.stepper.StepVerificationState
 import com.nerdstone.neatform.R
 import com.nerdstone.neatformcore.domain.builders.FormBuilder
 import com.nerdstone.neatformcore.form.json.JsonFormBuilder
 
 
-class FormActivity : AppCompatActivity() {
+class FormActivity : AppCompatActivity(), StepperActions {
     private lateinit var mainLayout: LinearLayout
     private lateinit var pageTitleTextView: TextView
     private lateinit var exitFormImageView: ImageView
@@ -41,6 +44,37 @@ class FormActivity : AppCompatActivity() {
                 )
                 JsonFormBuilder(mainLayout, path).buildForm(views)
             }
+
+            (formBuilder as JsonFormBuilder).neatStepperLayout?.stepperActions = this
         }
+    }
+
+    override fun onStepError(stepVerificationState: StepVerificationState) {
+    }
+
+    override fun onButtonNextClick(step: Step) {
+    }
+
+    override fun onButtonPreviousClick(step: Step) {
+    }
+
+    override fun onStepComplete(step: Step) {
+        Toast.makeText(this, "Stepper completed", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onExitStepper() {
+        val confirmCloseDialog = AlertDialog.Builder(this)
+        confirmCloseDialog.apply {
+            setTitle("Confirm close")
+            setMessage("All the unsaved data will get lost if you quit")
+            setPositiveButton("Exit") { _, _ -> finish() }
+            setNegativeButton("Cancel") { _, _ -> return@setNegativeButton }
+            create()
+        }
+        confirmCloseDialog.show()
+    }
+
+    override fun onCompleteStepper() {
+        Toast.makeText(this, "Completed entire step", Toast.LENGTH_SHORT).show()
     }
 }
