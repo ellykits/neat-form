6bceb2a3096d6d466bb3b15f5800cd30e1efd8be
cozej4 <ilakozejumanne@gmail.com>
2019-11-28 13:12:00 +0300

    Refactored custom views

--- neat-form-core/src/main/java/com/nerdstone/neatformcore/utils/Utils.kt
+++ neat-form-core/src/main/java/com/nerdstone/neatformcore/utils/Utils.kt
@@ -1,6 +1,8 @@
 package com.nerdstone.neatformcore.utils
 
+import android.R
 import android.content.Context
+import android.os.Build
 import android.util.TypedValue
 import android.view.View
 import android.view.inputmethod.InputMethodManager
@@ -31,7 +33,7 @@ object Utils {
         return isRequired == "yes" || isRequired == "true"
     }
 
-    fun hideSoftKeyBoard(view:View) {
+    fun hideSoftKeyBoard(view: View) {
         val inputMethodManager =
             view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
